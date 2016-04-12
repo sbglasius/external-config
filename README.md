1a70199e22b400f3733e3291381c041a81a410b0
Søren Berg Glasius <soeren@glasius.dk>
2016-04-12 22:21:55 +0200

    First attempt to publish with travis

---
+++ README.md
@@ -0,0 +1,7 @@
+External-Config
+===============
+
+This plugin will mimic the Grails 2 way of handling external configurations defined in `grails.config.locations`.
+
+First you must define `grails.config.locations`. This can be done in either `application.yml` like this:
+
--- src/main/groovy/grails/plugin/externalconfig/ExternalConfigGrailsPlugin.groovy
+++ src/main/groovy/grails/plugin/externalconfig/ExternalConfigGrailsPlugin.groovy
@@ -19,15 +19,12 @@ Load configs with grails.config.locations like in Grails 2.x
     def documentation = "http://grails.org/plugin/external-config"
     def license = "APACHE"
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/sbglasius/external-config/issues" ]
 
     // Online location of the plugin's browseable source code.
-    //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/sbglasius/external-config" ]
 }
