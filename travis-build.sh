c400c920be59f692237b3093f9dca8a24d7b5e26
Søren Berg Glasius <soeren@glasius.dk>
2016-04-13 09:46:58 +0200

    Another attempt

--- travis-build.sh
+++ travis-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./gradlew clean test assemble
-
-filename=$(find build/libs -name "*.jar" | head -1)
-filename=$(basename "$filename")
 
 mkdir -p $HOME/.gradle
 echo >> $HOME/.gradle/gradle.properties "bintrayUser=$BINTRAY_USER"
@@ -12,6 +8,12 @@ echo >> $HOME/.gradle/gradle.proeprties "bintrayKey=$BINTRAY_KEY"
 echo >> $HOME/.gradle/gradle.proeprties "grailsPortalUser=$GRAILS_PORTAL_USER"
 echo >> $HOME/.gradle/gradle.proeprties "grailsPortalPassword=$GRAILS_PORTAL_PASSWORD"
 cat $HOME/.gradle/gradle.properties
+
+./gradlew clean test assemble
+
+filename=$(find build/libs -name "*.jar" | head -1)
+filename=$(basename "$filename")
+
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
