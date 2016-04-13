158880d1d249b3d4afbd2d657f02fb9db7354cf8
Søren Berg Glasius <soeren@glasius.dk>
2016-04-13 09:31:15 +0200

    Build that knows environment variables

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,6 @@ rm -rf *.zip
 filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
-echo $BINTRAY_USER
-
 EXIT_STATUS=0
 echo "Publishing archives for branch $TRAVIS_BRANCH"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
