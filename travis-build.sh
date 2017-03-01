47c8a7dbb39d91ff1b00593a89a4405719e80763
Søren Berg Glasius <soeren@glasius.dk>
2017-03-01 15:29:19 +0100

    Fix travis-build for PRs

--- travis-build.sh
+++ travis-build.sh
@@ -8,8 +8,9 @@ echo "Publishing archives for branch $TRAVIS_BRANCH"
 ./gradlew clean check install --stacktrace
 
 EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n ${TRAVIS_TAG} ]] || [[ ${TRAVIS_BRANCH} == 'master' && ${TRAVIS_PULL_REQUEST} == 'false' ]]; then
+# Only publish if the branch is on master, and it is not a PR
+if [[ -n ${TRAVIS_TAG} ]] || [[ ${TRAVIS_BRANCH} == 'master' && -n "${TRAVIS_PULL_REQUEST}" ]]; then
+  echo "Publishing archives for branch $TRAVIS_BRANCH"
   if [[ -n ${TRAVIS_TAG} ]]; then
     echo "Pushing build to Bintray"
     ./gradlew bintrayUpload -PpluginVersion=${TRAVIS_TAG}  || EXIT_STATUS=$?
