7432bba13e801ef704e2caf92ee154ea31506a3c
sdelamo <sergio.delamo@softamo.com>
2017-06-20 10:41:00 +0200

    started() was renamed starting()

--- src/main/groovy/grails/plugin/externalconfig/ExternalConfigRunListener.groovy
+++ src/main/groovy/grails/plugin/externalconfig/ExternalConfigRunListener.groovy
@@ -86,6 +86,9 @@ class ExternalConfigRunListener implements SpringApplicationRunListener {
 		new MapPropertySource(resource.filename, properties as Map)
 	}
 
+	// Spring Boot 1.4 or higher
+	void starting() { }
+	// Spring Boot 1.3
 	void started() { }
 	void contextPrepared(ConfigurableApplicationContext context) { }
 	void contextLoaded(ConfigurableApplicationContext context) { }
