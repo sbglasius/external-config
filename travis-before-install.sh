2517e10cf9de2785cde739678ad544e4c31bad94
Søren Berg Glasius <soeren@glasius.dk>
2018-08-16 11:32:55 +0200

    Build on jdk7 instead of jdk8 - fix .travis.yml

--- travis-before-install.sh
+++ travis-before-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+set -ex
+
+echo "Preparing BountyCastel Security provider"
 # Workaround to using openjdk7 with Gradle due to security issue:
 # https://github.com/gradle/gradle/issues/2421
 # as described here: https://github.com/gradle/gradle/issues/2421#issuecomment-327838985
@@ -7,9 +9,9 @@ BCPROV_FILENAME=bcprov-ext-jdk15on-158.jar
 BCPROV_PATH=$HOME/.bountycastel
 BCPROV_FULL_PATH=${BCPROV_PATH}/${BCPROV_FILENAME}
 mkdir -p ${BCPROV_PATH}
-if [ ! -f ${BCPROV_FULL_PATH} ]; then # only get file if not cached
+#if [ ! -f ${BCPROV_FULL_PATH} ]; then
     curl https://bouncycastle.org/download/${BCPROV_FILENAME} -o ${BCPROV_FULL_PATH}
-fi
+#fi
 sudo cp ${BCPROV_FULL_PATH} /usr/lib/jvm/java-7-openjdk-amd64/jre/lib/ext
 sudo perl -pi.bak -e 's/^(security\.provider\.)([0-9]+)/$1.($2+1)/ge' /etc/java-7-openjdk/security/java.security
 echo "security.provider.1=org.bouncycastle.jce.provider.BouncyCastleProvider" | sudo tee -a /etc/java-7-openjdk/security/java.security
